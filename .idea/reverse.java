94b89fbd63627b266d386db1428ab39476ae6733
DnyaneshwarGanlewar <121995132+DnyaneshwarGanlewar@users.noreply.github.com>
2023-04-19 00:46:06 +0000

    ReverseNumber

---
+++ .idea/reverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class reverse {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt(); 
+        int reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit; 
+            num /= 10;
+        }
+
+        System.out.println("The reversed number is: " + reversed);
+    }
+}
